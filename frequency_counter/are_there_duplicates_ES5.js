790248e6ea4daeb3c8faba8b831a6a72e9edd826
bbtwots <brettbelka@gmail.com>
2020-10-13 20:19:21 -0700

    are there duplicates

---
+++ frequency_counter/are_there_duplicates_ES5.js
@@ -0,0 +1,26 @@
+function areThereDuplicates(arguments) {
+console.log("hih");
+    //create counter object
+    let counter = {};
+
+    //for each arugment
+    for (let i = 0; i < arguments.length; i++) {
+        console.log("in");
+
+        const arg = arguments[i];
+        //create entry in counter
+        counter[arg] = (counter[arg] || 0) + 1;
+        console.log(counter);
+        //if duplicate, return true
+        if (counter[arg] > 1) {
+            console.log("true");
+            return true;
+        }
+    }
+
+    //if no duplicates arise, return false
+    // console.log("false");
+    // return false;
+}
+
+areThereDuplicates(1, 2, 3, 4, 5, 3)
