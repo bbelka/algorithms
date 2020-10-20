7ba00fa6bc09604190c36b1154ec113705cfb496
bbtwots <brettbelka@gmail.com>
2020-10-19 19:13:17 -0700

    few quick ones

---
+++ odd_or_even.js
@@ -0,0 +1,16 @@
+// Write code to return the string "odd" if `num` is an odd number
+// and return the string "even" if `num` is an even number
+
+var oddOrEven = function (num) {
+    
+    if (num % 2 === 0) {
+        console.log("even");
+    }
+    else {
+        console.log("odd");
+    }
+};
+
+oddOrEven(1);
+oddOrEven(2);
+oddOrEven(517);
