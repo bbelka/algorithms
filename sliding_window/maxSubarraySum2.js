18c87c5919e820275d423089b4bf9838a196bd47
bbtwots <brettbelka@gmail.com>
2021-11-22 22:02:26 -0800

    initial solution

--- sliding_window/maxSubarraySum2.js
+++ sliding_window/maxSubarraySum2.js
@@ -4,11 +4,28 @@
 
 const maxSubarraySum = (arr, num) => {
 
+    if (num > arr.length) return null;
+
+    let temp = 0;
+    let max = 0;
+
+    for (let i = 0; i < num; i++) {
+        temp += arr[i];
+    }
+    max = temp;
+
+    for (let j = num; j < arr.length; j++) {
+        temp -= arr[j - num];
+        temp += arr[j];
+        if (temp > max) max = temp;
+    }
+
+    return max;
 
 }
 
 console.log(maxSubarraySum([100, 200, 300, 400], 2));       //700
 console.log(maxSubarraySum([1, 4, 2, 10, 23, 3, 1, 0, 20], 4))   //39;
 console.log(maxSubarraySum([-3, 4, 0, -2, 6, -1], 2));        //5
-console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1],));  //5
+console.log(maxSubarraySum([3, -2, 7, -4, 1, -1, 4, -2, 1],2));  //5
 console.log(maxSubarraySum([2, 3], 3));                   //null
