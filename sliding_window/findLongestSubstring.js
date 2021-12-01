00b3af0dcaa0c83808e1cc4e03d498ee42e10dcb
bbtwots <brettbelka@gmail.com>
2021-11-30 21:56:20 -0800

    uniqueInOrder

--- sliding_window/findLongestSubstring.js
+++ sliding_window/findLongestSubstring.js
@@ -1,41 +1,25 @@
 //Write a function which accepts a string and returns the length of the longest substring with all distinct characters.
 
-const findLongestSubstring = (s) => {
-    if (s.length === 0) return 0;
-    if (s.length === 1) return 1;
+const findLongestSubstring = (str) => {
+    let max = 0;
+    let start = 0;
+    let seen = new Object();
 
-    let l = 0;
-    let r = 1;
-    let len = 0;
-    let currLen = 0;
-    let map = new Object();
-
-    map[s[l]] = 1
-
-    while (r < s.length) {
-        if (map[s[r]] >= 1) {
-            Object.keys(map).forEach(key => {
-                map[key] = 0;
-            })
-            currLen = 0;
-            l = r;
-            r = l + 1;
-        } else {
-            map[s[r]] = 1;
-            currLen = r - l + 1;
-            r++;
-        };
-        if (currLen > len) len = currLen;
-
-
+    for (let i = 0; i < str.length; i++) {
+        if (seen[str[i]]) {
+            start = Math.max(start, seen[str[i]]);
+            seen[str[i]] = i;
+        }
+        max = Math.max(max, i - start + 1);
+        seen[str[i]] = i + 1;
     }
-    return len;
-}
+    return max;
+};
 
 console.log(findLongestSubstring(''));
 console.log(findLongestSubstring('rithmschool'));//7
 console.log(findLongestSubstring('thisisawesome'));//6
 console.log(findLongestSubstring('thecatinthehat'));//7
 console.log(findLongestSubstring('bbbbbb'));//1
-console.log(findLongestSubstring('longestsubstring'));//9
+console.log(findLongestSubstring('longestsubstring'));//8
 console.log(findLongestSubstring('thisishowwedoit'));//6
