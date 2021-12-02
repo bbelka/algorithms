495022a21cf3eb5a0c8c8291e44d3885fe6640e8
bbtwots <brettbelka@gmail.com>
2021-12-02 15:45:15 -0800

    comp2

--- code_wars/compareArrays.js
+++ code_wars/compareArrays.js
@@ -1,20 +1,28 @@
 //Given two arrays a and b write a function comp(a, b) (orcompSame(a, b)) that checks whether the two arrays have the "same" elements, with the same multiplicities (the multiplicity of a member is the number of times it appears). "Same" means, here, that the elements in b are the elements in a squared, regardless of the order.
 
+// const comp = (a1, a2) => {
+//     if (!a1 || !a2) return false;
+//     const map1 = new Object;
+//     const map2 = new Object;
+//     a1.forEach(el => {
+//         if (map1[Math.pow(el, 2)]) map1[Math.pow(el, 2)]++;
+//         else map1[Math.pow(el, 2)] = 1;
+//     });
+//     a2.forEach(el => {
+//         if (map2[el]) map2[el]++;
+//         else map2[el] = 1;
+//     });
+//     for (const key in map1) if (map1[key] !== map2[key]) return false;
+//     for (const key in map2) if (map1[key] !== map2[key]) return false;
+//     return true;
+// };
+
 const comp = (a1, a2) => {
-    if (!a1 || !a2) return false;
-    const map1 = new Object;
-    const map2 = new Object;
-    a1.forEach(el => {
-        if (map1[Math.pow(el, 2)]) map1[Math.pow(el, 2)]++;
-        else map1[Math.pow(el, 2)] = 1;
-    });
-    a2.forEach(el => {
-        if (map2[el]) map2[el]++;
-        else map2[el] = 1;
-    });
-    for (const key in map1) if (map1[key] !== map2[key]) return false;
-    for (const key in map2) if (map1[key] !== map2[key]) return false;
-    return true;
+    if (Array.isArray(a1) == false || Array.isArray(a2) == false) return false;
+    if (a1.length !== a2.length) return false;
+    a1.sort((a, b) => a - b);
+    a2.sort((a, b) => a - b);
+    return a1.every((el, i) => el * el === a2[i])
 };
 
 console.log(comp([121, 144, 19, 161, 19, 144, 19, 11], [121, 14641, 20736, 361, 25921, 361, 20736, 361]));
